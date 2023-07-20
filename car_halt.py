cff08aa8bce7ffa6c800cff06f9d3c60669bffe5
Aaditya Narayan <89546041+BrutalBotX@users.noreply.github.com>
2023-07-20 18:54:58 +0530

    Update car_halt.py

--- car_halt.py
+++ car_halt.py
@@ -1,6 +1,5 @@
 import os
 import system
-import curses
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
@@ -9,10 +8,6 @@ GPIO.setup(11,GPIO.OUT)
 GPIO.setup(38,GPIO.OUT)
 GPIO.setup(40,GPIO.OUT)
 
-screen = curses.initscr()
-curses.noecho() 
-curses.cbreak()
-screen.keypad(True)
 pkill -f car_back.py
 pkill -f car_left.py
 pkill -f car_right.py
@@ -30,8 +25,6 @@ try:
                 GPIO.output(40,False)
              
 finally:
-    curses.nocbreak(); screen.keypad(0); curses.echo()
-    curses.endwin()
     GPIO.cleanup()
     
 
