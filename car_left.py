8f5e32741e305e4abeeba3ae309f857ba9404e1e
Aaditya Narayan <89546041+BrutalBotX@users.noreply.github.com>
2023-07-20 18:51:55 +0530

    Create car_left.py

---
+++ car_left.py
@@ -0,0 +1,32 @@
+import os
+import system
+import curses
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7,GPIO.OUT)
+GPIO.setup(11,GPIO.OUT)
+GPIO.setup(38,GPIO.OUT)
+GPIO.setup(40,GPIO.OUT)
+
+screen = curses.initscr()
+curses.noecho() 
+curses.cbreak()
+screen.keypad(True)
+pkill -f car_forward.py
+pkill -f car_back.py
+pkill -f car_right.py
+pkill -f car_halt.py
+
+try:
+                GPIO.output(7,False)
+                GPIO.output(11,True)
+                GPIO.output(38,True)
+                GPIO.output(40,False)
+             
+finally:
+    curses.nocbreak(); screen.keypad(0); curses.echo()
+    curses.endwin()
+    GPIO.cleanup()
+    
+
